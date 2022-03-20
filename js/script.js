52178aa409391ad7dd35f789093e82d0e6bd3b98
juliamwangi123 <julia.mwangi@student.moringaschool.com>
2022-03-20 16:51:34 +0300

    ft:add the numbering oder in relation to pizza size

--- js/script.js
+++ js/script.js
@@ -17,11 +17,12 @@ Order.prototype.total = function(){
     return `${this.pizzaPrice + this.toppingsPrice + this.crustPrice}`
 };
 
-
+//add button on small size pizza
 
  let smallbtn = document.querySelector(".btn1");
  
-
+let smallBtnCnt = document.getElementById("totalBill")
+smallBtnCnt= 0+1;
  smallbtn.addEventListener("click", (e)=>{
      e.preventDefault();
      let num=  document.getElementById("num")
@@ -50,12 +51,13 @@ Order.prototype.total = function(){
  
      let newOrder = new Order(800,optionSe2, optionSel3);
  
-     bill.innerHTML +=`Small size pizza  with ${toppingsText} topping and ${crustText} crust  @ ${newOrder.total()}</br>`;
+     bill.innerHTML +=`  ${smallBtnCnt++} Small size pizza  with ${toppingsText} toppings and ${crustText} crust  @ ${newOrder.total()}</br>`;
  
      
      
  });
 
+//add button on medium size pizza
 
  let mediumBtn = document.querySelector(".medium");
  let mediumCount =0;
@@ -87,11 +89,14 @@ Order.prototype.total = function(){
     let newOrder = new Order(800,optionSe2, optionSel3);
     let total = `${mediumCount * newOrder.total()}`
 
-    bill.innerHTML +=`medium size pizza  with ${toppingsText} topping and ${crustText} crust  @ ${newOrder.total()}</br>`;
+    bill.innerHTML +=`${mediumCount++} medium size pizza  with ${toppingsText} toppings and ${crustText} crust  @ ${newOrder.total()}</br>`;
 
     
     
 });
+
+//add button on large size pizza
+
 
 let largeBtn =document.querySelector(".large");
 let largeCount =0;
@@ -126,6 +131,6 @@ largeBtn.addEventListener("click",(e)=>{
 
     let newOrder = new Order(1000,optionSe2, optionSel3);
     // let total = `${largeCount * newOrder.total()}`
-    bill.innerHTML +=`large size pizza with ${toppingsText} topping and ${crustText} crust  @ ${newOrder.total()}</br>`;
+    bill.innerHTML +=`${largeCount++} large size pizza with ${toppingsText} toppings and ${crustText} crust  @ ${newOrder.total()}</br>`;
 } )
  
